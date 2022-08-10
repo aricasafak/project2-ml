ed7f208a1524f1d20f41a270eef2a5bd50feb62e
aricasafak <49718743+aricasafak@users.noreply.github.com>
2022-08-10 10:53:36 -0500

    update

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 from flask import Flask, request
 from tensorflow import keras
-import numpy as np
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras import layers
+import cv2
+import json
+
+def load_image(image_path):
+    image = cv2.imread(image_path)
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+def process(img):
+    return cv2.resize(img/255.0, (512, 512)).reshape(-1, 512, 512, 3)
+def predict(img):
+    return model.layers[2](model.layers[1](model.layers[0](process(img)))).numpy()[0]
 
 app = Flask(__name__)
+model = keras.models.load_model('modelplant.hdf5', custom_objects={'FixedDropout':layers.Dropout})
+
+
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
     args = request.args
+    img = load_image(args.get("imgName") + '.jpg')
+    preds = predict(img)
+    return "healthy: " + str(round(preds[0] * 100, 2)) + ", " + "scab: " + str(round(preds[1] * 100, 2)) + ", " + "rust: " + str(round(preds[2] * 100, 2)) + ", " + "multiple_diseases: " + str(round(preds[3] * 100, 2))
 
-    label_dict = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Neutral', 5: 'Sad', 6: 'Surprise'}
-
-    model = keras.models.load_model('model_optimal.h5')
-
-    test_img = image.load_img(args.get("imagePath")+'.jpg', target_size=(224, 224), color_mode="grayscale")
-    test_img = np.expand_dims(test_img, axis=0)
-    test_img = test_img.reshape(1, 224, 224, 1)
-    result = model.predict(test_img)
-
-    result = list(result[0])
-    img_index = result.index(max(result))
-    return label_dict[img_index]
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    app.run()
 
